8c2785bd9141142fea93edd6054c728cb4a5284b
ConstruAcerte <nsrimes@gmail.com>
2025-08-29 09:38:32 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { setupDemoData } from './utils/demoDataSetup.js'
+
+// Configurar dados de demonstração na primeira execução
+setupDemoData();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
